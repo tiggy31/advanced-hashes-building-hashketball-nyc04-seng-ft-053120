3c1a4742cb708e0ee789a758532e3ae931dc1394
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 14:30:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
 {
    :home =>{:team_name =>"Brooklyn Nets",
             :colors => ["Black", "White"],
-            :players => [{:player_name => "Alan Anderson","Reggie Evans","Brook Lopez","Mason Plumlee, "Jason 
+            :players => [{:player_name => "Alan Anderson","Reggie Evans","Brook Lopez","Mason Plumlee", "Jason Terry"
                           :number => 0,30,11,1,31,
                           :shoe =>16,14,17,19,15,
                           :points =>22,12,17,26,19,
