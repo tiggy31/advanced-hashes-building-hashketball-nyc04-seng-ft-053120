e9b7d7c7aaa505dd9e78eb40e907a2acd4820401
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 14:54:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,7 +19,7 @@ def game_hash
             :colors => ["Turquoise", "Purple"],
             :players => [
               
-              
+                  { :player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5 },
               
               
               
