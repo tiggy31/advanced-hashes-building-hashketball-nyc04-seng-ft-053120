6fab635c841819504f751b2057580b23a8aa767f
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 15:01:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,7 +20,7 @@ def game_hash
             :players => [
                  { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 },
                  
-  }
+  
 
                  
               
