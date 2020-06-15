ff4b9652ffaa64725d2fe7c3a581ef3900579ca4
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 13:54:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,34 +1,6 @@
 # Write your code here!
 def game_hash
   
-  {
-    :home =>{:team_name => "Brooklyn Nets"
-             :colors => ["Black","White"]
-             :players => [{:player_name =>
-                           :number =>
-                           :shoe =>
-                           :points =>
-                          :rebounds =>
-                          :assists =>
-                           :steals =>
-                           :blocks =>
-                           :slam_dunks =>
-                          }]
-    }
-    :away =>{:team_name =>"Charlotte Hornets"
-             :colors => [ "Turquoise", "Purple"]
-             :players[{:player_name =>
-                       :number =>
-                       :shoe =>
-                       :points =>
-                       :rebounds =>
-                       :assists =>
-                       :steals =>
-                       :blocks =>
-                       :slam_dunks =>
-                      }]
-    }
-  
-  }
+ {}
   
 end
