7a3d07789b499f142ec1616b617119de3063a257
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 15:00:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,7 +18,10 @@ def game_hash
    :away =>{ :team_name =>"Charlotte Hornets",
             :colors => ["Turquoise", "Purple"],
             :players => [
+                 { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 },
                  
+  }
+
                  
               
               
