2fc6674178b713bb71a6833663854ddfbf1d6ed0
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 14:51:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,15 +4,7 @@ def game_hash
 {
    :home =>{:team_name =>"Brooklyn Nets",
             :colors => ["Black", "White"],
-            :players => [{:player_name => "Alan Anderson",
-                          :number =>0,
-                          :shoe =>16,
-                          :points =>22,
-                          :rebounds =>12,
-                          :assists =>12,
-                          :steals =>3,
-                          :blocks =>1,
-                          :slam_dunks =>1}]
+            :players => [{ :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1 }
                           
                            
                              
