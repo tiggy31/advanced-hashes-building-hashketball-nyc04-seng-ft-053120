bb7fbe6554416d0c1748ece3f048da5bb18aa800
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 14:09:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,9 @@
 def game_hash
   
 {
-   :home =>{:team_name   
-            :colors
-            :players
+   :home =>{:team_name =>"",
+            :colors => [""],
+            :players => [{}]
      
    }, 
    :away =>{ }
