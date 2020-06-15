c47f4f7933335314ede375ad27a42ca70edb38a8
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 14:36:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,17 @@ def game_hash
                           :slam_dunks =>1
                            }
             
-            
+                        {
+                          :player_name => "Reggie Evans",
+                          :number =>30,
+                          :shoe =>14,
+                          :points =>12,
+                          :rebounds =>12,
+                          :assists =>12,
+                          :steals =>12,
+                          :blocks =>12,
+                          :slam_dunks =>1
+                        }
             
             ]
      
