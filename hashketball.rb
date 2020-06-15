8b496e8036ceb4f4d2362e86bb975f6182e7d890
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 14:15:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
      
    }, 
    :away =>{ :team_name =>"Charlotte Hornets",
-            :colors => [""],
+            :colors => ["Turquoise", "Purple"],
             :players => [{}]
      
    }
