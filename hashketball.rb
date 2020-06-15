9c7187d42538398618c2619264db979a83db3bcd
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-15 15:01:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,7 +22,7 @@ def game_hash
                  
                  { :player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10 },
                  
-
+                { :player_name => "DeSagna Diop", :number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5 },
                  
               
               
